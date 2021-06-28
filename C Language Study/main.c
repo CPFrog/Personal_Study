91a5e9a1c168577cea75c9b9db917cbe8e5bede4
CP개구링 <kokk012@naver.com>
2021-06-28 11:26:28 +0900

    Xcode commit test

---
+++ C Language Study/main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  C Language Study
+//
+//  Created by CP개구링 on 2021/06/28.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("Hello, World!\n");
+    return 0;
+}
