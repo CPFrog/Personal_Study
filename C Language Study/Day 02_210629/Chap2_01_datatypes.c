e788f0019425994bb311d3c3f93a71c2ca4a5eb5
CP개구링 <kokk012@naver.com>
2021-06-29 11:45:04 +0900

    210629_02_Datatypes

---
+++ C Language Study/Day 02_210629/Chap2_01_datatypes.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    printf("%d\n",10); //정수형 상수 (integer constant)
+    printf("%d %d\n", 10,20);
+    printf("%lf\n",3.14); //실수형 상수 (float constant)
+    printf("%c\n",'a'); //문자형 상수 (character constant)
+    printf("%s\n","sky"); //문자열 상수 (string constant)
+    printf("10\n");
+    return 0;
+}
