51d625719b7cadda24fe2638b4a8659974406836
CP개구링 <35889115+CPFrog@users.noreply.github.com>
2021-07-01 08:50:45 +0900

    210701_JSP_QueryExtract

--- JSP Study/JSP_Study/src/main/java/com/edu/study/QueryTestServlet.java
+++ JSP Study/JSP_Study/src/main/java/com/edu/study/QueryTestServlet.java
@@ -18,6 +18,27 @@ public class QueryTestServlet extends HttpServlet {
 		out.print("<body>");
 		out.print("<h1>GET 방식으로 요청되었습니다.</h1>");
 		out.println("</body></html>");
+		
+		String id=req.getParameter("id");
+		String pw=req.getParameter("pwd");
+		String name=req.getParameter("name");
+		String[] hobbies=req.getParameterValues("hobby");
+		String gender=req.getParameter("gender");
+		String religion=req.getParameter("reqligion");
+		String selfpr=req.getParameter("selfpr");
+		
+		out.print("ID: "+id+"<br/>");
+		out.print("PW: "+pw+"<br/>");
+		out.print("이름: "+name+"<br/>");
+		out.print("취미: ");
+		for(String i:hobbies)
+			out.print(i+" ");
+		out.print("<br/>");
+		out.print("성별: "+gender+"<br/>");
+		out.print("종교: "+religion+"<br/>");
+		out.print("자기소개: "+selfpr+"<br/>");
+		out.print("전체 질의 문자열: "+req.getQueryString());
+		out.println("</body></html>");
 		out.close();
 	}
 
@@ -29,6 +50,29 @@ public class QueryTestServlet extends HttpServlet {
 		out.print("<body>");
 		out.print("<h1>POST 방식으로 요청되었습니다.</h1>");
 		out.println("</body></html>");
+		
+		String id=req.getParameter("id");
+		String pw=req.getParameter("pwd");
+		String name=req.getParameter("name");
+		String[] hobbies=req.getParameterValues("hobby");
+		String gender=req.getParameter("gender");
+		String religion=req.getParameter("reqligion");
+		String selfpr=req.getParameter("selfpr");
+		
+		out.print("ID: "+id+"<br/>");
+		out.print("PW: "+pw+"<br/>");
+		out.print("이름: "+name+"<br/>");
+		out.print("취미: ");
+		for(String i:hobbies)
+			out.print(i+" ");
+		out.print("<br/>");
+		out.print("성별: "+gender+"<br/>");
+		out.print("종교: "+religion+"<br/>");
+		out.print("자기소개: "+selfpr+"<br/>");
+		out.print("전체 질의 문자열: "+req.getQueryString());
+		out.println("</body></html>");
 		out.close();
 	}
+	
 }
+	
