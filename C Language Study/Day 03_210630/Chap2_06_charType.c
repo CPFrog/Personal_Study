e4d1e3229122999096f73bfb0b856583c817236a
CP개구링 <kokk012@naver.com>
2021-06-30 11:54:32 +0900

    210630_06&07_char&string

---
+++ C Language Study/Day 03_210630/Chap2_06_charType.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    printf("문자 상수 A = %c\n",'A');
+    printf("문자 상수 A의 정수형 상수(ASCII Code) = %d\n", 'A');
+    printf("97 정수형 상수(ASCII Code)의 문자 상수 = %c\n", 97);
+    
+    return 0;
+}
---
+++ C Language Study/Day 03_210630/Chap2_07_stringType.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    printf("문자열 apple의 길이 : %d\n", sizeof("apple"));
+    printf("문자열 sandwitch의 길이 : %d\n", sizeof("sandwitch"));
+    
+    printf("%c\n", 'A');
+    printf("%s\n", "apple");
+    return 0;
+}
