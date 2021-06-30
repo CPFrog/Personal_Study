d0b7cbaa86037181f8cb7d72659a1774df0141f1
CP개구링 <35889115+CPFrog@users.noreply.github.com>
2021-06-30 16:51:10 +0900

    210630_JSP_AdditionalInfoServlet

---
+++ JSP Study/JSP_Study/src/main/java/com/edu/study/AdditionalInfoServlet.java
@@ -0,0 +1,27 @@
+package com.edu.study;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/moreInfos")
+
+public class AdditionalInfoServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException{
+		res.setContentType("text/html;charset=UTF-8");
+		var out=res.getWriter();
+		out.print("<html>");
+		out.print("<head><title>Request 정보 출력</title></head>");
+		out.print("<body>");
+		out.print("<h3>추가적인 요청 정보</h3>");
+		out.print("Request Method: "+req.getMethod()+"</br>");
+		out.print("Path Info: "+req.getPathInfo()+"</br>");
+		out.print("Path Translated: "+req.getPathTranslated()+"</br>");
+		out.print("Query String: "+req.getQueryString()+"</br>");
+		out.print("Content Length: "+req.getContentLength()+"</br>");
+		out.print("Content Type: "+req.getContentType()+"</br>");
+		out.print("</body></html>");
+		out.close();
+	}
+}
