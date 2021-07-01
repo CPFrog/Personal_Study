cd1955abd8d56b52f0d0db4ee0db6903a0747691
CP개구링 <kokk012@naver.com>
2021-07-01 10:52:34 +0900

    210701_C_stringIntroduction

--- C Language Study/Day 04_210701/Basic02_10_stringArray.c
+++ C Language Study/Day 04_210701/Basic02_10_stringArray.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-//문자열 넣어보기
+//문자열 변수
 int main(){
 	//	1. 변수 선언 + 2. 초기화
-	//	char pnum="AB01", pname="apple"; <- 문자 변수에 문자열을 넣으려고 하므로 에러 발생
+	char pnum[5]="AB01", pname[]="apple";
 	int price=12000, count=100;
 	float height=30.7, weight=20.6;
 	
 	// 3. 변수 실행문
-//	printf("제품번호 : %c\n", pnum);
-//	printf("제품명 : %c\n", pname);
+	printf("제품번호 : %s\n", pnum);
+	printf("제품명 : %s\n", pname);
 	printf("가격 : %d\n", price);
 	printf("주문 수량 : %d\n",count);
 	printf("높이 : %lf\n", height);
