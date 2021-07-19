8c0559476cfd5cfb9678852a3a102761998e738c
CP개구링 <kokk012@naver.com>
2021-07-19 11:47:28 +0900

    210719_C_customHeader

--- C Language Study/Day 16_210719/lab09-03.cpp
+++ C Language Study/Day 16_210719/lab09-03.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "User.h"
 #define SQUARE(x) x*x
 #define PRT(x) printf("%d\n",x)
 
@@ -6,7 +7,7 @@ int main(){
     int x=4;
     
     PRT(SQUARE(x+2));
-    PRT(100/SQUARE(2));
+    PRT(MAX/SQUARE(2));
     PRT(SQUARE(++x));
     return 0;
 }
