2e4ccbc7575462e57807a93dafb59c1585a47e90
CP개구링 <35889115+CPFrog@users.noreply.github.com>
2021-07-28 17:54:08 +0900

    210728_JSP_customTag
    실행 제어 2 : SKIP_BODY
    (execution control 2)

--- JSP Study/JSP_Study/src/main/java/com/edu/customTag/MyCustomTag.java
+++ JSP Study/JSP_Study/src/main/java/com/edu/customTag/MyCustomTag.java
@@ -7,7 +7,9 @@ public class MyCustomTag extends TagSupport{
 	@Override
 	public int doStartTag() throws JspException {
 		System.out.println("시작 태그 만남");
-		return EVAL_BODY_INCLUDE;
+		//return super.doStartTag();
+		//return EVAL_BODY_INCLUDE; <- 메소드 종료 후 몸체가 실행되므로 example23의 1+2 결과 3이 출력됨.
+		return SKIP_BODY; // 메소드 종료 후 몸체를 실행하지 않고 건너뛰므로 body 문 실행 안되고 바로 종료태그 실행.
 	}
 	
 	@Override
