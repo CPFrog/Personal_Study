cec1c6ff9616e855c3b825d4d05af25fc3aa51a0
CP개구링 <35889115+CPFrog@users.noreply.github.com>
2021-07-29 12:48:42 +0900

    210729_JSP_customTag
    속성 정의 : color
    (Attribute Define)

--- JSP Study/JSP_Study/src/main/java/com/edu/customTag/MyCustomTag.java
+++ JSP Study/JSP_Study/src/main/java/com/edu/customTag/MyCustomTag.java
@@ -5,20 +5,32 @@ import javax.servlet.jsp.tagext.TagSupport;
 
 public class MyCustomTag extends TagSupport{
 	int cnt=1;
+	private String color;
+	
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color=color;
+	}
 	
 	@Override
 	public int doStartTag() throws JspException {
 		
 		
 		System.out.println("시작 태그 만남");
+		System.out.println("Color 속성 : "+color);
 		//return super.doStartTag();
 		//return EVAL_BODY_INCLUDE; <- 메소드 종료 후 몸체가 실행되므로 example23의 1+2 결과 3이 출력됨.
 		//return SKIP_BODY; 메소드 종료 후 몸체를 실행하지 않고 건너뛰므로 body 문 실행 안되고 바로 종료태그 실행.
 		
+		return EVAL_BODY_INCLUDE;
+		/*
 		if(cnt++<10) // 조건문을 둠으로써 무한루프에 빠지지 않도록 설정.
 			return EVAL_BODY_AGAIN; 
 		else
 			return SKIP_BODY;
+			*/
 	}
 	
 	@Override
