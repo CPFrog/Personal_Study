8edc98c8d4cc1a5f388bbccdc3ea7c061dedad90
CP개구링 <35889115+CPFrog@users.noreply.github.com>
2021-07-29 18:08:02 +0900

    210729_JSP_customTag
    태그 기반 커스텀태그 - 태그 지시자
    (Tag Based customTag - Tag Indicator)

--- JSP Study/JSP_Study/src/main/java/com/edu/customTag/MyCustomTag2.java
+++ JSP Study/JSP_Study/src/main/java/com/edu/customTag/MyCustomTag2.java
@@ -8,7 +8,8 @@ import javax.servlet.jsp.tagext.SimpleTagSupport;
 public class MyCustomTag2 extends SimpleTagSupport {
 	
 	@Override
-	public void doTag() throws JspException, IOException {
+	//JSP 1.2에서 doStartTag(), doAfterBody(), doEndTag()로 실행됐던 것이 2.0 이후로는 doTag()하나로 처리
+	public void doTag() throws JspException, IOException { 
 		System.out.println("커스텀 태그의 바디가 실행되기 전");
 		for(int i=0;i<10;i++)
 			getJspBody().invoke(null);
---
+++ JSP Study/JSP_Study/src/main/webapp/chapter13/example25.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="my" tagdir="/WEB-INF/tags" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>tag 지시자</title>
+</head>
+<body>
+
+<my:line/>
+오늘 날짜는 : <my:time/>
+<my:line/>
+
+</body>
+</html>
