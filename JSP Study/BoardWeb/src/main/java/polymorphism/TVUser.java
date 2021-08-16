0c3c3a928c71a4d16000d72a11fdd826c0c993ce
CP개구링 <35889115+CPFrog@users.noreply.github.com>
2021-08-16 14:30:32 +0900

    210816_Spring_Container
    Day 1_Chapter 3.1

--- JSP Study/BoardWeb/src/main/java/polymorphism/TVUser.java
+++ JSP Study/BoardWeb/src/main/java/polymorphism/TVUser.java
@@ -1,24 +1,22 @@
 package polymorphism;
 
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class TVUser {
 	public static void main(String[] args) {
-//		SamsungTV tv=new SamsungTV();
-//		tv.powerON();
-//		tv.channelUP();
-//		tv.channelDown();
-//		tv.powerOFF();
+		//Spring 컨테이너 구동
+		AbstractApplicationContext factory=
+				new GenericXmlApplicationContext("applicationContext.xml");
 		
-//		LGtv tv=new LGtv();
-//		tv.turnON();
-//		tv.chUP();
-//		tv.chDown();
-//		tv.turnOFF();
-		
-		BeanFactory factory=new BeanFactory();
-		TV tv=(TV)factory.getBean(args[0]);
+		//Spring 컨테이너로부터 필요한 객체 요청(Lookup)
+		TV tv=(TV)factory.getBean("tv");
 		tv.powerOn();
 		tv.channelUp();
 		tv.channelDown();
 		tv.powerOff();
+		
+		//Spring 컨테이너 종료
+		factory.close();
 	}
 }
