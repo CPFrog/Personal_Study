ad22e4d6b425bca1a0720e12b56ca25a5a851af6
CP개구링 <35889115+CPFrog@users.noreply.github.com>
2021-08-16 15:23:39 +0900

    210816_Spring_Constructor
    Day 1_Chapter 4.2.0

--- JSP Study/BoardWeb/src/main/java/polymorphism/SamsungTV.java
+++ JSP Study/BoardWeb/src/main/java/polymorphism/SamsungTV.java
@@ -1,10 +1,15 @@
 package polymorphism;
 
 public class SamsungTV implements TV{
-	private SonySpeaker speaker=new SonySpeaker();
+	private SonySpeaker speaker;
 	
 	public SamsungTV() {
+		speaker=new SonySpeaker();
 		System.out.println("~~ SamsungTV 객체 생성 ~~");
+	}
+	public SamsungTV(SonySpeaker speaker) {
+		System.out.println("~~ SamsungTV_2 객체 생성");
+		this.speaker=speaker;
 	}
 	public void powerOn() {
 		System.out.println("SamsungTV--전원 ON");
