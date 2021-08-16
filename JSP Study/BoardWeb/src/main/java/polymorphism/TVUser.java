7ff285b53d043733dfe29fe30e8a3f765389b056
CP개구링 <35889115+CPFrog@users.noreply.github.com>
2021-08-16 14:45:31 +0900

    210816_Spring_ElementConfig
    Day 1_Chapter 3.2.4

--- JSP Study/BoardWeb/src/main/java/polymorphism/TVUser.java
+++ JSP Study/BoardWeb/src/main/java/polymorphism/TVUser.java
@@ -10,11 +10,9 @@ public class TVUser {
 				new GenericXmlApplicationContext("applicationContext.xml");
 		
 		//Spring 컨테이너로부터 필요한 객체 요청(Lookup)
-		TV tv=(TV)factory.getBean("tv");
-		tv.powerOn();
-		tv.channelUp();
-		tv.channelDown();
-		tv.powerOff();
+		TV tv1=(TV)factory.getBean("tv");
+		TV tv2=(TV)factory.getBean("tv");
+		TV tv3=(TV)factory.getBean("tv");
 		
 		//Spring 컨테이너 종료
 		factory.close();
