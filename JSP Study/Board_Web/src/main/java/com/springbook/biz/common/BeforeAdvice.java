137e0cb1c9e18b1b64dcbae83744d103e3488171
CP개구링 <35889115+CPFrog@users.noreply.github.com>
2021-08-30 17:29:56 +0900

    210830_Spring_JoinPoint
    Day 2_Chapter 4.2

--- JSP Study/Board_Web/src/main/java/com/springbook/biz/common/BeforeAdvice.java
+++ JSP Study/Board_Web/src/main/java/com/springbook/biz/common/BeforeAdvice.java
@@ -1,8 +1,12 @@
 package com.springbook.biz.common;
 
+import org.aspectj.lang.JoinPoint;
+
 public class BeforeAdvice {
-	public void beforeLog() {
-		System.out.println("[Before] 비즈니스 로직 수행 전");
+	public void beforeLog(JoinPoint jp) {
+		String method = jp.getSignature().getName();
+		Object[] args=jp.getArgs();
+		System.out.println("[Before] "+method+"() 메소드 Arguments 정보 : "+args[0].toString());
 	}
 
 }
