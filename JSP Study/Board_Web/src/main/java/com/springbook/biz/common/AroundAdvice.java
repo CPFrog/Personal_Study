5cc52305228cd10b7dcc9b87d64b226269d84f40
CP개구링 <35889115+CPFrog@users.noreply.github.com>
2021-09-28 17:58:43 +0900

    210928_Spring_JoinPoint
    Day 2_Chapter 4.5 Around Advice

--- JSP Study/Board_Web/src/main/java/com/springbook/biz/common/AroundAdvice.java
+++ JSP Study/Board_Web/src/main/java/com/springbook/biz/common/AroundAdvice.java
@@ -1,14 +1,20 @@
 package com.springbook.biz.common;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.util.StopWatch;
 
 public class AroundAdvice {
 	public Object aroundLog(ProceedingJoinPoint pjp) throws Throwable {
-		System.out.println("[BEFORE] : 메소드 수행 전");
-		Object returnObj = pjp.proceed();
-		System.out.println("[AFTER] : 메소드 수행 후");
-
-		return returnObj;
+		String method=pjp.getSignature().getName();
+		
+		StopWatch sw= new StopWatch();
+		sw.start();
+		
+		Object obj = pjp.proceed();
+		
+		sw.stop();
+		System.out.println(method+" 수행 소요 시간 : "+sw.getTotalTimeMillis()+"(ms)초");
+		return obj;
 	}
 
 }
